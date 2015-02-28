c39a2f38e7d17a577874d0d49b12d9dec7b89806
An Wu <anwu@andrew.cmu.edu>
2015-02-28 18:41:21 -0500

    I2C starter code

--- uart_test_328p/uart_test.c
+++ uart_test_328p/uart_test.c
@@ -40,6 +40,63 @@ FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 FILE uart_io = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
 
+// I2C code
+void TWIInit(void)
+{
+   //set SCL to 400kHz
+   TWSR = 0x00;
+   TWBR = 0x0C;
+   //enable TWI
+   TWCR = (1<<TWEN);
+}
+
+// Start signal
+void TWIStart(void)
+{
+   TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+   while ((TWCR & (1<<TWINT)) == 0);
+}
+
+// Stop signal
+void TWIStop(void)
+{
+   TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+}
+
+// I2C write
+void TWIWrite(uint8_t u8data)
+{
+   TWDR = u8data;
+   TWCR = (1<<TWINT)|(1<<TWEN);
+   while ((TWCR & (1<<TWINT)) == 0);
+}
+
+// I2C read
+uint8_t TWIReadACK(void)
+{
+   TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+   while ((TWCR & (1<<TWINT)) == 0);
+   return TWDR;
+}
+
+// I2C read
+// (read byte with NACK)
+uint8_t TWIReadNACK(void)
+{
+   TWCR = (1<<TWINT)|(1<<TWEN);
+   while ((TWCR & (1<<TWINT)) == 0);
+   return TWDR;
+}
+
+// I2C get status
+uint8_t TWIGetStatus(void)
+{
+   uint8_t status;
+   //mask status
+   status = TWSR & 0xF8;
+   return status;
+}
+
 int main(void)
 {
 
